46488824b2043e85376b3141b96661f32f199ccc
Jeova Jerusalen <jeovajerusalen@gmail.com>
2026-06-30 15:17:48 -0600

    Fix deploy script: git identity local para gh-pages

--- scripts/deploy-pages.mjs
+++ scripts/deploy-pages.mjs
@@ -26,6 +26,8 @@ cpSync(dist, deployDir, { recursive: true })
 writeFileSync(join(deployDir, '.nojekyll'), '')
 
 execSync('git init', { cwd: deployDir, stdio: 'inherit' })
+execSync('git config user.email "deploy@procesador-imagenes-ia.local"', { cwd: deployDir, stdio: 'inherit' })
+execSync('git config user.name "Procesador Imagenes IA Deploy"', { cwd: deployDir, stdio: 'inherit' })
 execSync('git checkout -b gh-pages', { cwd: deployDir, stdio: 'inherit' })
 execSync('git add -A', { cwd: deployDir, stdio: 'inherit' })
 execSync('git commit -m "Deploy GitHub Pages"', { cwd: deployDir, stdio: 'inherit' })
